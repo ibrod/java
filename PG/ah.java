000eb6b2885a5afe52cf33ef0a64010e6c242c50
bmrf <1316489465@qq.com>
2021-10-04 11:00:50 +0800

    ah

---
+++ PG/ah.java
@@ -0,0 +1,10 @@
+package PG;
+
+public class ah {
+    public int a;
+    public int b;
+
+    public static void pf() {
+        System.out.println("sdads");
+    }
+}
--- kewu.java
+++ kewu.java
@@ -1,5 +1,11 @@
+import java.util.*;
+
+import PG.ah;
 public class kewu{
     public static void main(String[] args) {
-        
+        ah.pf();
+        ah good=new ah();
+        good.a=6;
+        System.out.println(good.a);
     }
 }
