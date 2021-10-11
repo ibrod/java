f10cec796c6af261ddf48eeec5d9d268e923509c
ibrod <1316489465@qq.com>
2021-10-11 10:59:34 +0800

    hao

--- kewu.java
+++ kewu.java
@@ -1,14 +1,27 @@
-
 import java.util.*;
 import PG.ah;
-public class kewu{
-    public static void main(String[] args) {
+/*
+    
+*/
+
+class azhe{
+     public int kkkwu;
+     static int asd=456;
+     void fdsf(String[] args) {
         ah.pf();
+        kkkwu=666;
         ah good=new ah();
+        kkkwu=6796;
         boolean a=true;
         good.a=6;
         System.out.println(good.a);
     }
-    
+}
+
+public class kewu{
+    public static void main(String[] args) {
+        azhe.asd=9999;
+        
+    }
 }
 
