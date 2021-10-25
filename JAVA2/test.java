be5bdeb233144d9e2a5d591e566f81e4bdfe527f
bmrf <1316489465@qq.com>
2021-10-25 11:22:35 +0800

    hao

---
+++ JAVA2/test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        show(fac.getGoods(sc.next()));
+    }
+
+    public static void show(goods gds){
+        gds.get_price();
+    }
+
+}
