b3da6fa460c4e12f9e10b747e4be9533c27cc043
ibrod <1316489465@qq.com>
2022-09-12 23:49:11 +0800

    version2.0

---
+++ javafxqq/qq_gui/src/hellofx/Gui.java
@@ -0,0 +1,44 @@
+package hellofx;
+import javafx.application.Application;
+import javafx.geometry.Point3D;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.control.Label;
+
+public class Gui extends Application {
+    @Override
+    public void start(Stage stage) {
+        try {
+        StackPane root = new StackPane();
+        Label label=new Label("This is my first javafx program.");
+        label.setStyle("-fx-font-size: 20px; -fx-text-fill: red;");
+        label.setTranslateX(-50);
+        label.setTranslateY(-50);
+        // label.setTranslateZ(50);
+        //以上是调整坐标的方法
+        label.setText("This label has been changed.");
+        // label.setScaleX(0.5);
+        // label.setScaleY(0.5);
+        // label.setScaleZ(0.5);
+        //以上是缩放
+
+        Circle circle=new Circle(5,5,5,Color.RED);
+        circle.setStroke(Color.BLACK);
+        root.getChildren().add(label);
+        root.getChildren().add(circle);
+        //create a scene
+        Scene scene = new Scene(root,400,200);
+        stage.setTitle("whatever");
+        stage.setScene(scene);
+        stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
