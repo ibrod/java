8e72c2ea371bdfc17958c64d51ab95be2a24f26b
ibrod <1316489465@qq.com>
2022-09-13 18:51:28 +0800

    finished

--- QQ_login_gui/QQ_login_gui/src/code/login_gui.java
+++ QQ_login_gui/QQ_login_gui/src/code/login_gui.java
@@ -19,56 +19,56 @@ public class Login_gui extends Application {
         try {
             //背景图片
             Image image = new Image("E:\\code\\java\\QQ_login_gui\\QQ_login_gui\\src\\resources\\bkg.jpg");
-            ImageView imageView = new ImageView(image);
+            ImageView background = new ImageView(image);
 
             //QQ头像
             Image image2 = new Image("E:\\code\\java\\QQ_login_gui\\QQ_login_gui\\src\\resources\\QQ_icon.png");
-            ImageView imageView2 = new ImageView(image2);
-            imageView2.relocate(50, 230);
+            ImageView qq_icon = new ImageView(image2);
+            qq_icon.relocate(50, 230);
 
             //QQ号码输入框
-            TextField textField = new TextField();
-            textField.setPrefSize(240, 29);
-            textField.relocate(160, 250);
+            TextField qqnumber = new TextField();
+            qqnumber.setPrefSize(240, 29);
+            qqnumber.relocate(160, 250);
 
             //密码输入框
-            PasswordField passwordField = new PasswordField();
-            passwordField.setPrefSize(240, 29);
-            passwordField.relocate(160, 280);
+            PasswordField password = new PasswordField();
+            password.setPrefSize(240, 29);
+            password.relocate(160, 280);
 
             //记住密码
-            CheckBox checkBox = new CheckBox("记住密码");
-            checkBox.relocate(160, 315);
+            CheckBox remember_password = new CheckBox("记住密码");
+            remember_password.relocate(160, 315);
 
             //自动登录
-            CheckBox checkBox2 = new CheckBox("自动登录");
-            checkBox2.relocate(300, 315);
+            CheckBox auto_login = new CheckBox("自动登录");
+            auto_login.relocate(300, 315);
 
             //注册账号
-            Label label = new Label("注册账号");
-            label.relocate(410, 250);
-            label.setFont(new javafx.scene.text.Font(15));
-            label.setTextFill(Color.web("#0000FF"));
+            Label register = new Label("注册账号");
+            register.relocate(410, 250);
+            register.setFont(new javafx.scene.text.Font(15));
+            register.setTextFill(Color.web("#0000FF"));
 
             //找回密码
-            Label label2 = new Label("找回密码");
-            label2.relocate(410, 285);
-            label2.setFont(new javafx.scene.text.Font(15));
-            label2.setTextFill(Color.web("#0000FF"));
+            Label forget_password = new Label("找回密码");
+            forget_password.relocate(410, 285);
+            forget_password.setFont(new javafx.scene.text.Font(15));
+            forget_password.setTextFill(Color.web("#0000FF"));
 
             //登录按钮
-            Button button = new Button("登录");
-            button.relocate(160, 350);
-            button.setPrefSize(240, 29);
-            button.setStyle("-fx-background-color: #00BFFF; -fx-text-fill: #FFFFFF;");
-            button.setFont(new javafx.scene.text.Font(15));
+            Button login = new Button("登录");
+            login.relocate(160, 350);
+            login.setPrefSize(240, 29);
+            login.setStyle("-fx-background-color: #00BFFF; -fx-text-fill: #FFFFFF;");
+            login.setFont(new javafx.scene.text.Font(15));
 
             //创建一个Pane
             Pane pane = new Pane();
-            pane.getChildren().addAll(imageView2, textField,passwordField,checkBox,checkBox2,label,label2,button);
+            pane.getChildren().addAll(qq_icon, qqnumber,password,remember_password,auto_login,register,forget_password,login);
             //创建一个StackPane
             StackPane root = new StackPane();
-            root.getChildren().addAll(imageView,pane);
+            root.getChildren().addAll(background,pane);
             //创建一个scene
             Scene scene = new Scene(root, 530, 400);
             //创建一个stage
@@ -79,7 +79,6 @@ public class Login_gui extends Application {
             e.printStackTrace();
         }
     }
-    
     public static void main(String[] args) {
         launch(args);
     }
