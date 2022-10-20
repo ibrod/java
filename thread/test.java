3bc8434fa7dec8826559c720400bcfbe3f6c7dbb
bmrf <1316489465@qq.com>
2022-10-20 17:15:46 +0800

    dsadadad

--- thread/test.java
+++ thread/test.java
@@ -1,5 +1,6 @@
 public class test {
     public String ps;
+
     public static void main(String[] args) {
         Thread2 t1 = new Thread2("https://img1.doubanio.com/view/subject/l/public/s32279258.jpg","1.jpg");
         Thread2 t2 = new Thread2("https://img-blog.csdnimg.cn/20201210142514843.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3NlcmVuZGlwaXR5X3hy,size_16,color_FFFFFF,t_70","2.jpg");
@@ -10,5 +11,3 @@ public class test {
         System.out.println("hhh");
 
     }
-
-}
